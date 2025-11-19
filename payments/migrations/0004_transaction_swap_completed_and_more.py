4a583c260425dde53b84e83755273dc216e3d9f1
CeosTech <ceostech.dev@gmail.com>
2025-11-19 22:15:26 +0100

    update migrations

--- payments/migrations/0004_transaction_swap_completed_and_more.py
+++ payments/migrations/0004_transaction_swap_completed_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('payments', '0002_alter_transaction_usdc_received'),
+        ('payments', '0003_alter_transaction_usdc_received'),
     ]
 
     operations = [
